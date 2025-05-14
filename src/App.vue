bdc749d8fb2a69c4b0de5a3ccfa5e5c47555e179
253551 <253551@163.com>
2025-05-14 22:31:40 +0800

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,47 @@
-<   模板>
-  < header class = “   标题”   >  < header class = “    header”   >
-    <部分class =“ main”> < section class = “     主”     >
-      <H1> <h1>​ ​​​
-        <img src =“ ./资产/images/logo.webp”/>
-        PAYQRCODE
-      </ h1 >      </ h1 >
-      <h2>二合一收款码物理合并版</h2><h2>二合一收款码物理合并版</h2>
-    </ pection >      </ pection >
-  </ header> </ header>
-  <ain> <ain>​ ​​​​ ​​​​ ​​​​ ​​​​ ​​​​ ​​​
-    <t-space方向=“垂直”>
-      <t-alert主题=“ info”>
-        <模板#message>
+<template>
+  <header class="header">
+    <section class="main">
+      <h1>
+        <img src="./assets/images/logo.webp" />
+        PayQrcode
+      </h1>
+      <h2>二合一收款码物理合并版</h2>
+    </section>
+  </header>
+  <main>
+    <t-space direction="vertical">
+      <t-alert theme="info">
+        <template #message>
           开源：
-          <t-link <t-link
-            主题=“主”
-            强调
-            href = “   https：
-            target =“ _空白”
+          <t-link
+            theme="primary"
+            underline
+            href="https://github.com/uxiaohan/PayQrcode"
+            target="_blank"
           >
-            PAYQRCODE
-          </ t-link >
+            PayQrcode
+          </t-link>
           ，技术支持：
-          <t-link主题=“ priendar”下划线href =“ https://www.longs.cn” target =“ _ blank”>
+          <t-link theme="primary" underline href="https://www.longs.cn" target="_blank">
             心动次元
-          </ t-link >
-        </ template >
-      </ t-alert >
-      <t-alert主题=“成功”>
-        <模板#icon> </template>
-        <模板#message>
-          <p>​​
+          </t-link>
+        </template>
+      </t-alert>
+      <t-alert theme="success">
+        <template #icon></template>
+        <template #message>
+          <p>
             ✅ 物理合并，直接将
             图片合并成一张图片，无需服务器，不怕被篡改，可打印使用，稳定，绿色，安全！
-          </ p >
+          </p>
           <p>⚠️全程离线运行，二维码解析合并无任何更改，且项目开源，请放心食用！</p>
-        </ template >
-      </ t-alert>
+        </template>
+      </t-alert>
       <t-alert theme="warning" message="请尽量把非二维码区域裁剪后上传，以提升识别准确性！" />
-    </T空间>
-    < main />
-  </ main >
-</ template >
-<脚本lang =“ TS”设置>
-从“@/组件/main/main.vue”导入主
-<// script>
+    </t-space>
+    <Main />
+  </main>
+</template>
+<script lang="ts" setup>
+import Main from '@/components/Main/Main.vue'
+</script>
